316d530f30104d0519c3be2c34565c97618bc7be
raphaelli <raphael_li@live.com>
2019-05-28 14:12:55 +0800

    demo translate

---
+++ README.md
@@ -0,0 +1,11 @@
+# Static Web Page Generator
+
+Based on the .net core platform, the static web page generator reads and parses the text in MarkDown format and generates static Html page content.
+
+基于 .net core 平台的，静态网页生成器，读取解析 MarkDown 格式文本，并生成静态Html页面内容。
+
+# Milestone
+
+- 5/28/2019  0.0.1 测试完成文本转换
+
+## TODO
--- dotNetStaticWebBuilder/Program.cs
+++ dotNetStaticWebBuilder/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using Markdig;
+using System.IO;
 
-namespace dotNetStaticWebBuilder
+namespace MarkdigTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String line;
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(@"D:\Code\MarkdigTest\TestFile.md"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    line = sr.ReadToEnd();
+                    Console.WriteLine("——————预览读取的内容————————");
+                    Console.WriteLine(line);
+                }
+
+                var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                var result = Markdown.ToHtml(line, pipeline);
+
+                // Set a variable to the Documents path.
+                string docPath = @"D:\Code\MarkdigTest\";
+                //Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write the string array to a new file named "WriteLines.txt".
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.html")))
+                {
+                    //foreach (string line in lines)
+                    outputFile.WriteLine(result);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
